8f2db4d8d87cbde34492446bac525fc626c9d508
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-19 13:30:34 +0100

    Update tester.py

--- python-0-string/ex03/tester.py
+++ python-0-string/ex03/tester.py
@@ -2,15 +2,15 @@
 from NULL_not_found import NULL_not_found
 
 Nothing = None
-Garlic = float("NaN")
+Garlic = float("Nan")
 Zero = 0
 Empty = ''
 Fake = False
 
-NULL_not_found(Nothing)  # Should print <class 'NoneType'> and return 0
-NULL_not_found(Garlic)   # Should print <class 'float'> and return 0
-NULL_not_found(Zero)     # Should print <class 'int'> and return 0
-NULL_not_found(Empty)    # Should print <class 'str'> and return 0
-NULL_not_found(Fake)     # Should print <class 'bool'> and return 0
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
 
-print(NULL_not_found("Brian"))  # Should print <class 'str'> and return 1
