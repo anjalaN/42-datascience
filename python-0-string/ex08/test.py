ebacf9bc06f42c5451d313d449bad65e9c60427f
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-19 13:20:43 +0100

    Update test.py

--- python-0-string/ex08/test.py
+++ python-0-string/ex08/test.py
@@ -1,13 +1,11 @@
+#!/usr/bin/env python3
 from time import sleep
-from tqdm import tqdm  # This is the original tqdm
-from Loading import ft_tqdm  # This is your custom implementation
+from tqdm import tqdm
+from Loading import ft_tqdm
 
-# Test ft_tqdm (your function)
 for elem in ft_tqdm(range(333)):
     sleep(0.005)
 print()
-
-# Test original tqdm
 for elem in tqdm(range(333)):
     sleep(0.005)
 print()
