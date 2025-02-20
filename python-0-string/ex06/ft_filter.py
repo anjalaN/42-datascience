94316dc18bc2b25746253f9314e44f085920f2f5
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-20 15:32:28 +0100

    Update ft_filter.py

--- python-0-string/ex06/ft_filter.py
+++ python-0-string/ex06/ft_filter.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def ft_filter(function, iterable):
-    """Return an iterator yielding those items of iterable for which 
-    function(item) is true."""
+    """
+filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+s true. If function is None, return the items that are true
+    """
     if function is None:
         return (item for item in iterable if item)
     else:
