a6dd749d379eb21170b10104333ab193993671d5
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-20 12:37:04 +0100

    Update Hello.py

--- python-0-string/ex00/Hello.py
+++ python-0-string/ex00/Hello.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
-#list 
 ft_list = ["Hello", "tata!"]
 ft_list[1] = "World!"
 
-#tuple before convertis to list for modification
 ft_tuple = ("hello", "toto!")
 list = list(ft_tuple)
 list[1] = "France!"
 ft_tuple = tuple(list)
 
-#set
 ft_set = {"Hello", "tutu"}
 ft_set.remove("tutu")
 ft_set.add("Paris!")
 
-#dictionart
 ft_dict = {"Hello": "titi"}
 ft_dict["Hello"] = "42Paris!"
 
@@ -22,4 +18,3 @@ print(ft_list)
 print(ft_tuple)
 print(ft_set)
 print(ft_dict)
-
