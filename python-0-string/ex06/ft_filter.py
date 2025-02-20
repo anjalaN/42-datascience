4c812cfbd72a056760e24685f27e826c716a5651
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-20 14:23:54 +0100

    Update ft_filter.py

--- python-0-string/ex06/ft_filter.py
+++ python-0-string/ex06/ft_filter.py
@@ -1,48 +1,18 @@
-  
 #!/usr/bin/env python3
-import sys
-
-def world_longer_than_n(s, n):
-    words = s.split()  # Split string into words
-    result = [word for word in words if len(word) > n]  # Filter words longer than n
-    return result
-
-if __name__ == "__main__":
-    try:
-        # Check if the number of arguments is exactly 3
-        assert len(sys.argv) == 3, "AssertionError: the arguments are bad"
-        
-        s = sys.argv[1]  # First argument should be the string
-        assert not s.isdigit(), "AssertionError: the first argument must be a string"
-        
-        n = int(sys.argv[2])  # Second argument should be an integer
-    except (AssertionError, ValueError) as e:
-        # Print the error message and exit if there's an issue
-        print(e)
-        sys.exit(1)
-    
-    # Use list comprehension with a lambda function for filtering
-    filtered_words = list(filter(lambda word: len(word) > n, s.split()))
-    
-    # Print the filtered and sorted words
-    print(filtered_words)
------------------------------------------------------------
-#!/usr/bin/env python3
-import sys
-
 def ft_filter(function, iterable):
-      
+    """Return an iterator yielding those items of iterable for which 
+    function(item) is true."""
     if function is None:
         return (item for item in iterable if item)
     else:
         return (item for item in iterable if function(item))
 
+
 # Exemple d'utilisation
 def is_odd(num):
     return num % 2 == 0
 
+
 numbers = [1, 2, 3, 4, 5, 6]
 result = ft_filter(is_odd, numbers)
-print(list(result))  
-
-
+print(list(result))
