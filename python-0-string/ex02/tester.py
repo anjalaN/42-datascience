0a0a55419f97d97f6f44bbdf4988ba9ba9c61435
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-20 12:44:49 +0100

    Update tester.py

--- python-0-string/ex02/tester.py
+++ python-0-string/ex02/tester.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+
 from find_ft_type import all_thing_is_obj
 
-ft_list  = ["Hello", "tata!"]
+ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
-ft_set   = {"Hello", "tutu!"}
-ft_dict  = {"Hello": "titi!"}
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
 
 all_thing_is_obj(ft_list)
 all_thing_is_obj(ft_tuple)
@@ -12,5 +13,4 @@ all_thing_is_obj(ft_set)
 all_thing_is_obj(ft_dict)
 all_thing_is_obj("Brian")
 all_thing_is_obj("Toto")
-print(all_thing_is_obj(10))
-
+print(all_thing_is_obj(1.5))
