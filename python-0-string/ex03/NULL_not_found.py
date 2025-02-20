8eb755d3b8d914bc6998585c2b09bb4b25556da4
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2025-02-20 12:49:09 +0100

    Update NULL_not_found.py

--- python-0-string/ex03/NULL_not_found.py
+++ python-0-string/ex03/NULL_not_found.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 import math
 
+
 def NULL_not_found(object: any) -> any:
-    #handling None explicitly
+    # handling None explicitly
     if object is None:
         print(f"Nothing: None {type(object)}")
         return 0
-    #handle Nan (not a number) explicitly using math.isnan()
+    # handle Nan (not a number) explicitly using math.isnan()
     elif isinstance(object, float) and math.isnan(object):
         print(f"Cheese: nan {type(object)}")
         return 0
-    
-    #Handle false explicitly
+    # Handle false explicitly
     elif object is False:
         print(f"Fake: False {type(object)}")
         return 0
-    #handle zero explicitly
+    # handle zero explicitly
     elif object == 0:
         print(f"Zero: 0 {type(object)}")
         return 0
-    #handle empty
+    # handle empty
     elif object == '':
         print(f"Empty: {type(object)}")
         return 0
-   
     else:
-        print("Type not Found$")
+        print("Type not Found")
         return 1
-      
-
-
